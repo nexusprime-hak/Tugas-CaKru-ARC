7d76280049bf8f3759e2640bb1560cdd24eb099d
Devon Josiah <devon.anakotta@gmail.com>
2024-02-25 21:52:34 +0700

    Menambahkan file-file tugas Day 2

---
+++ Day 2/JS/links.js
@@ -0,0 +1,52 @@
+export const links = [ 
+	{ 
+		name: "Bercinta Lewat Kata - Donne Maula", 
+		link: "https://open.spotify.com/track/7wlfGt5NkpPT6nRC6yCr7u?si=c4af470494fb4722",
+		embed: '<iframe style="border-radius:12px" src="https://open.spotify.com/embed/track/7wlfGt5NkpPT6nRC6yCr7u?utm_source=generator" width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>'
+	}, 
+	{ 
+		name: "Mercusuar - Kunto Aji", 
+		link: "https://open.spotify.com/track/3fgdtD7nivCvPueM8UmvFg?si=75461c8b90af47fe",
+		embed: '<iframe style="border-radius:12px" src="https://open.spotify.com/embed/track/3fgdtD7nivCvPueM8UmvFg?utm_source=generator" width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>'
+	}, 
+	{ 
+		name: "Secrets of the World - Adhitita Sofyan", 
+		link: "https://open.spotify.com/track/3HqH8dM4VfUFONaC4BCNYQ?si=c5ccad0395734d53",
+		embed: '<iframe style="border-radius:12px" src="https://open.spotify.com/embed/track/3HqH8dM4VfUFONaC4BCNYQ?utm_source=generator" width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>' 
+	}, 
+	{ 
+		name: "Berdistraksi - Danilla", 
+		link: "https://open.spotify.com/track/55bmTrFYialBaZacYVejWw?si=c57785c784ea4230",
+		embed: '<iframe style="border-radius:12px" src="https://open.spotify.com/embed/track/55bmTrFYialBaZacYVejWw?utm_source=generator" width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>'
+	}, 
+	{ 
+		name: "Senandung Maaf - White Shoes and the Couples", 
+		link: "https://open.spotify.com/track/0dYebrX5uGJ5waQ2DmKcga?si=9256d35f6545446f",
+		embed: '<iframe style="border-radius:12px" src="https://open.spotify.com/embed/track/0dYebrX5uGJ5waQ2DmKcga?utm_source=generator" width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>'
+    },
+	{ 
+		name: "ゆめうつつ (Yume Utsutsu) - Lamp", 
+		link: "https://open.spotify.com/track/1GTC0DN9LGOdP0NdvoHyX7?si=4429ea8a10b141a0",
+		embed: '<iframe style="border-radius:12px" src="https://open.spotify.com/embed/track/1GTC0DN9LGOdP0NdvoHyX7?utm_source=generator" width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>'
+	}, 
+	{ 
+		name: "Blue Sky Collapse - Adhitia Sofyan", 
+		link: "https://open.spotify.com/track/3o4nMGqSQrvOPpR39O7wwC?si=974b4ab0cde34660",
+		embed: '<iframe style="border-radius:12px" src="https://open.spotify.com/embed/track/3o4nMGqSQrvOPpR39O7wwC?utm_source=generator" width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>'
+	}, 
+	{ 
+		name: "Ada di Sana - Danilla", 
+		link: "https://open.spotify.com/track/7e3JeMmuteFbUJuUjH1RfS?si=2fd25f27ffc045e9",
+		embed: '<iframe style="border-radius:12px" src="https://open.spotify.com/embed/track/7e3JeMmuteFbUJuUjH1RfS?utm_source=generator" width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>'
+	},
+    {
+        name: "Perkenalan Perdana - Monita Tahalea",
+        link: "https://open.spotify.com/track/7e3JeMmuteFbUJuUjH1RfS?si=2fd25f27ffc045e9",
+		embed: '<iframe style="border-radius:12px" src="https://open.spotify.com/embed/track/3jQDDgAtlFFAprIGWTWVtu?utm_source=generator" width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>'
+    }, 
+    {
+        name: "A Song of Us - MarcoMache",
+        link: "https://open.spotify.com/track/10C3YdatPuhm0dgrFst2G4?si=23c2ac3b13354301",
+		embed: '<iframe style="border-radius:12px" src="https://open.spotify.com/embed/track/10C3YdatPuhm0dgrFst2G4?utm_source=generator" width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>'
+    }
+];
